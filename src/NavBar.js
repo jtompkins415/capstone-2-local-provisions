6530ab084135bed1ec65ea821345f2784795a268
jtompkins415 <john.tompkins415@gmail.com>
2023-01-25 17:46:44 -0800

    Initialize Repository

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <>
+            <nav className="nav justify-content-center">
+                <a className="nav-link" href="#">Home</a>
+                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#">Contact</a>
+            </nav>
+        </>
+
+    )
+}
+
+export default NavBar
