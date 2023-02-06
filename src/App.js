5191b29f84ee92cee631328775c7aff6addb6993
jtompkins415 <john.tompkins415@gmail.com>
2023-02-06 11:38:56 -0800

    Start creating routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home'
 import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-    <header>
-      <NavBar />
-    </header>
+      <header>
+        <NavBar />
+      </header>
+    <BrowserRouter>  
+      <Routes>
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
