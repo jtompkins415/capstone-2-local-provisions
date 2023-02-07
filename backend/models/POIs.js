fecf39ccdb0d57681d4d337fac7e180081589574
jtompkins415 <john.tompkins415@gmail.com>
2023-02-07 15:23:47 -0800

    Create class for points of interest

---
+++ backend/models/POIs.js
@@ -0,0 +1,125 @@
+"use strict";
+
+const db = require("../db");
+const {sqlForPartialUpdate} = require("../helpers/sql");
+const {
+    NotFoundError,
+    UnauthorizedError,
+    BadRequestError
+} = require('../expressError');
+
+class POIs {
+   
+    /** Find all Points of Interest
+     * 
+     * Returns [{name, rating, type, url}, ...]
+     */
+
+    static async getAllPOI() {
+        const results = await db.query(
+            `SELECT name,
+                rating, 
+                type,
+                url
+            FROM POIs
+            ORDER BY rating`
+        );
+
+        return results.rows;
+
+    }
+
+    /** Add new Point of Interest
+     * 
+     * Returns { name, rating, type, url }
+     * 
+     * Throw BadRequestError on duplicates
+     */
+
+    static async addNewPOI({name, rating, type, url}) {
+        const duplicateCheck = await db.query(
+            `SELECT name,
+                rating,
+                type,
+                url
+            FROM POIs
+            WHERE name = $1`,
+            [name],
+        )
+
+        if(!duplicateCheck.rows[0]){
+            throw new BadRequestError(`Duplicate Location: ${name}`);
+        }
+
+        const result = await db.query(
+            `INSERT INTO 
+                POIs
+                (name,
+                 rating,
+                 type,
+                 url)
+                VALUES ($1, $2, $3, $4)
+                RETURNING name, rating, type, url`,
+                [name,
+                rating,
+                type,
+                url],
+        );
+
+        const poi = result.rows[0];
+
+        return poi;
+    };
+
+    /** Given an ID, pull information for a Point of Interest
+     * 
+     * Returns { name, rating, type, url }
+     * 
+     * Throws NotFoundError if POI is not found
+     */
+
+    static async getPOI(id) {
+        const poiRes = await db.query(
+            `SELECT id,
+                name,
+                rating,
+                type,
+                url
+            FROM POIs
+            WHERE id = $1`,
+            [id],
+        );
+
+        const poi = poiRes.rows[0];
+
+        if(!poi) throw new NotFoundError(`NO POI: ${poi.name}`);
+
+        return poi;
+    }
+
+    /** Update information for a Point of Interest
+     * 
+     * Partial Update - Doesn't require all data to update
+     * 
+     * Data can include:
+     * { name, rating, type, url }
+     * 
+     * Returns  { name, rating, type, url}
+     * 
+     * Throws NotFoundError if POI is not found
+     */
+
+    static async updatePOI(id, data) {
+        
+        const {setCols, values} = sqlForPartialUpdate(
+            data,
+            {
+                name: 'name',
+                rating: 'rating',
+                type: 'type',
+                url: 'url'
+            });
+    }
+}
+
+module.exports = POIs;
