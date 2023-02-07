d0bbbe65d7abc2543afeec9a9ee44609a9026551
jtompkins415 <john.tompkins415@gmail.com>
2023-02-06 17:06:05 -0800

    update dependency link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Home'
-import NavBar from './NavBar';
+import NavBar from './routes-nav/NavBar';
 
 function App() {
   return (
