a2ab077fd535fdcc0407616434d2649e792df692
jtompkins415 <john.tompkins415@gmail.com>
2023-02-07 09:22:55 -0800

    Write Post, Patch, Delete routes for user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,17 @@
 
 const express = require('express');
 const router = new express.Router();
-const {ensureAdmin} = require('../middleware/auth');
+const {ensureAdmin, ensureCorrectUserOrAdmin} = require('../middleware/auth');
 const User = require('../models/user');
+const {BadRequestError} = require('../expressError');
+const jsonschema = require('jsonschema')
 
 
 
- /** Get all users
+ /** GET: Get all users
+  * 
+  * Returns {users: [{username, firstName, lastName, email, isAdmin}, {...}]}
+  * 
   * permissions: admin only
   */
 
@@ -20,19 +25,90 @@ router.get('/', ensureAdmin, async (req, res, next) => {
     }
 });
 
-/** Register user
+/** POST: Add new user
  * 
- * Returns {} 
- * permissions: none
+ * Not a registraion endpoint. Allow admin to add user to DB
+ * 
+ * Returns {user: {username, firstName, lastName, email, isAdmin}} 
+ * permissions: Admin
 */
 
-router.post('/:username', async (req, res, next) => {
+router.post('/:username', ensureAdmin, async (req, res, next) => {
   try{
+    const validator = jsonschema.validate(req.body, userUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+
     const {username, password, firstName, lastName, email, isAdmin} = req.body
     const result = await User.register(username, password, firstName, lastName, email, isAdmin);
     
     return res.status(201).json(result.rows[0]);
   }catch(err) {
     return next(err);
+  }
+})
+
+/** GET: Get information on a single user
+ * 
+ * Returns {user: {username, firstName, lastName, email }}
+ * 
+ * permissions: Admin or Same user
+ */
+
+router.get('/:username', ensureCorrectUserOrAdmin, async (req, res, next) => {
+  try {
+    const {username} = req.params;
+    const result = await User.get(username);
+
+    return res.status(200).json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH: Update user information 
+ * 
+ *
+ * 
+ * Data can include:
+ *  {firstName, lastName, password, email. isAdmin}
+ * 
+ * Returns {user: {username, firstName, lastName, email, isAdmin}}
+ * 
+ * Permissions: Admin or Same user
+*/
+
+router.patch('/:username', ensureCorrectUserOrAdmin, async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, userUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const {username} = req.params
+
+    const result = await User.update(username, req.body)
+    return res.status(204).json({result});
+  } catch (err) {
+    return next(err)
+  }
+})
+
+/** DELETE: Delete user
+ * 
+ * Permissions: Admin or Same user
+ */
+
+router.delete('/:username', ensureCorrectUserOrAdmin, async (req, res, next) => {
+  try {
+    const {username} = req.params
+    await User.remove(username)
+    return res.status(204).json({deleted: username})
+  } catch (err) {
+    return next(err)
   }
 })
