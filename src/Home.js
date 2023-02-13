d277f938e2a4f2f5bbb8cedd5cb68767b3bee2fa
jtompkins415 <john.tompkins415@gmail.com>
2023-02-13 10:37:31 -0800

    add links to sign up and login pages

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,24 @@ import {
     Card,
     CardTitle,
     CardBody,
-    CardText
+    CardText,
+    Button
 } from 'reactstrap'
 
 const Home = () => {
     return (
-       <>
-        
-       </>
+        <div className='Home'>
+            <h1>Local Provisions</h1>
+            <h3>Enjoy your community!</h3>
+            <div className='Home-body'>
+                <Button
+                    href='/signup'
+                    tag='a'>Sign Up!</Button>
+                <Button
+                    href='/login'
+                    tag='a'>Login!</Button>
+            </div>
+        </div>
     )
 }
 
