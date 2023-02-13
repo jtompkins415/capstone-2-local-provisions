ff4eb009c41c449d20d4908162931fc4e9442d9c
jtompkins415 <john.tompkins415@gmail.com>
2023-02-13 10:37:44 -0800

    create location component

---
+++ src/Location.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react';
+import POICard from './POICard';
+
+const Location = ({name}) => {
+
+    return (
+        <div className="Location">
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Location;
